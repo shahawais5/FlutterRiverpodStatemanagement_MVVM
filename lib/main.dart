0eae6ce535bc5cb7ccb333930b5adb6a3c749dc4
Shah Awais <shahawais@Shah-iMac.local>
2025-05-13 22:54:52 +0500

    firebase-configuration-android/ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'constants/app_keys.dart';
 import 'helpers/router_helper.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  final user = FirebaseAuth.instance.currentUser;
+  print('Current user: $user');
   runApp(const ProviderScope(child: MyApp()));
 }
 
